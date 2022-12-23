5a06d084922f3fa3292795555b2a76f1d616decc
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-22 18:48:29 -0800

    UserSchema updated for User.js with firstName, lastName, email, password, picturePath, friends, location, occupation, viewedProfile, impressions, export default User

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
 
 // CONFIGURATIONS - Middleware, Packages
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+// ROUTES with files
+app.post("/auth/register", upload.single("picture"), register);
 
 // MONGOOSE Setup
 const PORT = process.env.PORT || 6001;
