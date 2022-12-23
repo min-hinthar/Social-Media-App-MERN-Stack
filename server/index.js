974c725821bd4f799caf1f957e34975ffa4f4585
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-22 21:12:24 -0800

    server/data/index.js mock user and posts data updated and imported into MongoDB database

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { createPost } from './controllers/posts.js';
 import { register } from './controllers/auth.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // CONFIGURATIONS - Middleware, Packages
 const __filename = fileURLToPath(import.meta.url);
@@ -60,5 +63,9 @@ mongoose
 })
 .then(() => {
     app.listen(PORT, () => console.log(`SERVER running at PORT ${PORT}`));
+
+    // ADD TO DATABASE ONLY ONCE!
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 })
 .catch((error) => console.log(`${error}: Could not connect to Server`));
