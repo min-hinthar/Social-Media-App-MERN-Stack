f6c438de751a5d33199f818f7b8984ab2bb8df9f
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-22 19:34:07 -0800

    controllers/auth.js updated for LOGGING IN, jwt.sign user id

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -39,3 +39,22 @@ export const register = async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 };
+
+// LOGGING IN Setup
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ message: "User does not exist. Please register new User." });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Password is invalid! Please try again." });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+};
