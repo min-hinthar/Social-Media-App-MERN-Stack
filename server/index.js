4176fd423c27e5b437ae52d76f2d8127790c94fb
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-22 20:17:49 -0800

    createPost route imported for uploading pictures with new posts

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import { createPost } from './controllers/posts.js';
 import { register } from './controllers/auth.js';
+import { verifyToken } from './middleware/auth.js';
 
 // CONFIGURATIONS - Middleware, Packages
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +43,12 @@ const upload = multer({ storage });
 
 // ROUTES with files
 app.post("/auth/register", upload.single("picture"), register);
+app.post('/posts', verifyToken, upload.single("picture"), createPost);
 
 // ROUTES Setup
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 // MONGOOSE Setup
 const PORT = process.env.PORT || 6001;
