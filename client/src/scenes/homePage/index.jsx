3913fa04c92d4e16af645150decf5c02fc3874d3
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-25 16:18:14 -0800

    AdverWidget created for displaying advertisements

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
@@ -31,7 +32,10 @@ const HomePage = () => {
                     
                 </Box>
                 {isNonMobileScreens && (
-                    <Box flexBasis='26%'></Box>
+                    <Box flexBasis='26%'>
+                        <AdvertWidget />
+                        <Box m='2rem 0' />
+                    </Box>
                 )}
             </Box>
         </Box>
