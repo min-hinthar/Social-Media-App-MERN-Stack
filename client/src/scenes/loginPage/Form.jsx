2cc589cc1280b6b7b16d7972be4c9cc8a6956e8c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-12-26 20:54:40 -0800

    login and register fetch url updated to deployed server: https://the-republic-social-media.onrender.com

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
         formData.append('picturePath', values.picture.name);
 
         const savedUserResponse = await fetch(
-            'http://localhost:3001/auth/register',
+            'https://the-republic-social-media.onrender.com',
             {
                 method: 'POST',
                 body: formData,
@@ -80,7 +80,7 @@ const Form = () => {
 
     const login = async (values, onSubmitProps) => {
         const loggedInResponse = await fetch(
-            'http://localhost:3001/auth/login',
+            'https://the-republic-social-media.onrender.com',
             {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
